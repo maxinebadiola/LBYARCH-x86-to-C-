fdd34a7f0faafcdf80085bf6f1ee2eca6826bfca
Maxine Badiola <maxine_badiola@dlsu.edu.ph>
2024-04-01 15:48:29 +0800

    UPDATED: main.c formatting

--- main.c
+++ main.c
@@ -32,7 +32,10 @@ int main() {
     int sizes[] = { 1048576 , 16777216, 1073741824 };
     //stores execution time for each function
     double timeC[90];
+    double time64[90];
     double totalC = 0.0;
+    double total64 = 0.0;
+    boolean noErrors = 1;
 
     for (int i = 0; i < 3; i++)
     {
@@ -41,30 +44,38 @@ int main() {
         float* B = malloc(n * sizeof(float));
         fillTest(A, B, n);
 
-        //sdot in C (run 30x)
         for (int j = 0; j < 30; j++) {
-            start = clock();
-            float sdotC = dotProductC(n, 0.0f, A, B);
-            end = clock();
-            timeC[j] = ((double)(end - start)) / CLOCKS_PER_SEC;
+            //sdotC
+            float sdotC = dotProductC(n, 0.0f, A, B, timeC, j);
             totalC += timeC[j];
+
+            //sdot64
+            // start = clock();
+            // float sdot64 = dotProduct64(n, 0.0f, A, B);
+            // end = clock();
+            // time64[j] = ((double)(end - start)) / CLOCKS_PER_SEC;
+            // total64 += time64[j];
+
+            // printf("n = %d, sdot64 = %.2f, t = %f\n", n, sdot64, time64[j]);
+
+            // //compare sdotC and sdot64
+            // if (sdotC != sdot64) {
+            //     printf("ERROR: mismatch at iteration %d: sdotC = %f, sdot64 = %f\n", j, sdotC, sdot64);
+            //     noErrors = 0;
+            // }
+            // else
+            //     printf("VALID: match at iteration %d: sdotC = %f, sdot64 = %f\n", j, sdotC, sdot64);
         }
-
-
-
-        //sdot in x64
-        // float sdot64 = dotProduct64(n, 0.0f, A, B);
-        //print x64 results
-        //TODO 
-
-        //TODO: compare sdot of both sdotC and sdot64
-
     }
 
     //PRINT AVERAGE (3 values of n, 30 times each = 90 total times)
     double averageC = totalC / 90;
+    double average64 = total64 / 90;
     printf("\n============================================================\n");
+    printf("Test run is %s\n", noErrors == 1 ? "VALID" : "INVALID");
     printf("Average execution time in C: %f\n", averageC);
+    printf("Average execution time in 64: %f\n", average64);
+    printf("============================================================\n");
 
     return 0;
 }
